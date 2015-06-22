6e324896be49a075430d15e9b34fd22b84f61ba7
Charlton Wilbur <cwilbur@chromatico.net>
2015-06-22 00:48:37 -0400

    deploy to heroku to get urls

--- app.js
+++ app.js
@@ -1,72 +1,58 @@
-'use strict';
+// models
 
-var async = require('async');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/guestbook');
+var Entry = require('/lib/entry.js');
+var User = require('./lib/user.js');
 
-var readline = require('readline');
-var io = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+// basic express setup, views
+
+var jade = require('jade');
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'jade');
+app.set('views', './templates');
+
+app.use(express.static(__dirname + '/public'));
+
+// authentication
+
+var passport = require('passport');
+
+passport.use(new GitHubStrategy({
+    clientID: GITHUB_CLIENT_ID,
+    clientSecret: GITHUB_CLIENT_SECRET,
+    callbackURL: "http://127.0.0.1:3000/auth/github/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    User.findOrCreate({
+      githubId: profile.id
+    }, function(err, user) {
+      return done(err, user);
+    });
+  }
+));
+
+
+
+
+
+var apiRouter = require('./lib/api-routes.js');
+app.use('/api', apiRouter);
+
+app.get('/', function(req, res) {
+  res.render('index', {
+    name: "Max",
+    message: 'Welcome to our contacts page! I hope you have a good stay.'
+  });
 });
 
-io.setPrompt('> ');
+var server = app.listen(3000, function() {
 
-var TTT = require('./lib/tic-tac-toe.js');
-var game = new TTT();
+  var host = server.address().address;
+  var port = server.address().port;
 
-var drawBoard = function(game) {
-  var cells = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-  cells.forEach(function(cell, i, array) {
-    array[i] = ' ' + (game.whoPlayedAt(cell) || cell) + ' ';
-  });
+  console.log('Example app listening at http://%s:%s', host, port);
 
-  return [
-    cells.slice(0, 3).join('|'),
-    '---+---+---',
-    cells.slice(3, 6).join('|'),
-    '---+---+---',
-    cells.slice(6, 9).join('|')
-  ].join('\n');
-};
-
-process.stdout.write('Welcome to Tic-Tac-Toe\n\n');
-
-var moveCount = 0;
-
-var gameShouldContinue = function() {
-  return !game.isOver();
-};
-
-var playTurn = function(done) {
-  var player = game.whoseTurn();
-
-  process.stdout.write(drawBoard(game));
-  process.stdout.write('\n\nIt is ' + player + '\'s turn\n\n');
-
-  io.question('Where do you wish to play?', function(answer) {
-    var num = parseInt(answer, 10);
-
-    if (game.isMoveLegal(player, num)) {
-      game.move(player, num);
-    } else {
-      process.stdout.write('That is not a legal move.\n');
-    }
-
-    if (game.hasWon(player)) {
-      process.stdout.write('Player ' + player + ' wins!\n\n');
-    } else if (game.boardIsFull()) {
-      process.stdout.write('The game is a tie.\n\n');
-    }
-
-    done();
-  });
-};
-
-var gameDone = function(done) {
-  process.stdout.write('Thank you for playing.\n');
-  io.close();
-};
-
-async.whilst(
-  gameShouldContinue,
-  playTurn,
-  gameDone);
+});
